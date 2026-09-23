0c775e9239101fe5ac0e40a5d7802535ddb9b532
canelito447 <sledesma@escuelalosandes.edu.ar>
2026-09-23 16:45:07 -0300

    nombre del cliente, su sueldo y un poco mas

--- examen.py
+++ examen.py
@@ -1,12 +1,19 @@
 # Examen práctico - Sistema de pedidos del kiosco
-# Nombre y apellido:
-# Curso:
+# Nombre y apellido:Sarah Ledesma Verdun
+# Curso:2do 2a
 #
 # IMPORTANTE:
 # Resolver el programa siguiendo las etapas indicadas en el README.md.
 # Realizar los commits y push cuando se indique.
-
-
+print ("===== KIOSCO ESCOLAR =====")
+nombre = input("nombre: ")
+plata_cliente = int(input("cuanto dinero tiene?(en pesos) "))
+print ("tenemos : 1. Agua a $700.      2. alfajor a $900.           3. tostado a $2200")
+pedido =  int(input("que le apetece?(1, 2 o 3)"))
+productos = ["Agua", "alfajor", "tostado"]
+precios = [700, 900, 2200]
+print ("usted pidio: ", productos[pedido-1], "y le costara $",precios[pedido-1])
+pedido_total = precios[pedido-1]
 
 # =========================
 # ETAPA 1 - INICIO
